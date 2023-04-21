92183ceeaab716c5bef9cea8fb497516196f4dce
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 15:47:08 -0500

    can now see behind the cube

--- src/script.js
+++ src/script.js
@@ -72,8 +72,9 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     // Update camera
-    camera.position.x = cursor.x * 10
-    camera.position.y = cursor.y * 10
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * 5 
     camera.lookAt(mesh.position)
 
 
