28f0e157fc69bcaad67346f03f78bd5a72ce3da5
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 15:39:51 -0500

    added custom controls, but cant see back of cube

--- src/script.js
+++ src/script.js
@@ -10,7 +10,6 @@ window.addEventListener('mousemove', (event)=>
 {
     cursor.x = event.clientX / sizes.width - 0.5
     cursor.y = - (event.clientY / sizes.height - 0.5)
-    console.log(cursor.y)
 }) 
 
 /**
@@ -73,8 +72,11 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     // Update camera
-    camera.position.x = cursor.x * 3
-    camera.position.y = cursor.y * 3
+    camera.position.x = cursor.x * 10
+    camera.position.y = cursor.y * 10
+    camera.lookAt(mesh.position)
+
+
     // Render
     renderer.render(scene, camera)
 
