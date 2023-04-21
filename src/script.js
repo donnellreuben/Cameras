d45a6e8455d6a249966cbe5c7deb781f0ca45b65
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 15:37:14 -0500

    commented out animation and added mouse move code. Trying to add custom controls

--- src/script.js
+++ src/script.js
@@ -1,5 +1,18 @@
 import * as THREE from 'three'
 
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event)=>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+    console.log(cursor.y)
+}) 
+
 /**
  * Base
  */
@@ -24,9 +37,21 @@ scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+
+// const aspectRatio = sizes.width / sizes.height
+// console.log(aspectRatio)
+// const camera = new THREE.OrthographicCamera
+// (
+//     -1 * aspectRatio, 
+//     1 * aspectRatio, 
+//     1, 
+//     -1, 
+//     0.1, 
+//     100
+// )
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -45,8 +70,11 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
 
+    // Update camera
+    camera.position.x = cursor.x * 3
+    camera.position.y = cursor.y * 3
     // Render
     renderer.render(scene, camera)
 
