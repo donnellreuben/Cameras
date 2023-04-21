f0ae91a1534d3633a1b3559b980fc7afa7aefec7
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-21 16:00:14 -0500

    importing access to Orbit Controls and don't need THREE

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+console.log(OrbitControls)
 
 // Cursor
 const cursor = {
@@ -72,10 +75,10 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     // Update camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 5 
-    camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5 
+    // camera.lookAt(mesh.position)
 
 
     // Render
